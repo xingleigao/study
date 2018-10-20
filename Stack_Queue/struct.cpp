c8ffd3f5d711c21697413d75f7210a5eba5208fb
gxl <GaoXinglei>
2018-10-21 01:37:48 +0800

    add Stack_Queue

---
+++ Stack_Queue/struct.cpp
@@ -0,0 +1,32 @@
+//SqStack
+	typedef struct
+	{
+		int data[maxSize];	//存放栈中元素，maxSize为已定义常量
+		int top;		//栈顶指针
+ 	}SqStack;			//顺序栈类型定义
+//LNode
+	typedef struct LNode
+	{
+		int data;		//数据域
+		struct LNode *next;	//指针域
+	}LNode;				//链栈结点定义
+	//Node stack is similar with LNode ;
+//sqlist queue
+	typedef struct
+	{
+		int data[maxSize];
+		int front;		//队首指针
+		int rear;		//队尾指针
+	}SqQueue;
+//Queue LNode
+	typedef struct QNode
+	{
+		int data;
+		struct QNode *next;
+	}QNode;
+//list Queue
+	typedef struct
+	{
+		QNode *front;
+		QNode *rear;
+	}LiQueue;
